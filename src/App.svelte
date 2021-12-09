4b0f99bb51c9ad72647417dd9e2523afe66c3681
Joe Stevens <joemac79@gmail.com>
2021-12-08 20:34:48 -0500

    swap threejs for babylon

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,25 @@
+
 <script>
   import { onMount } from 'svelte';
   import { createScene } from "./scene";
-
   let el;
-
   onMount(() => {
     createScene(el)
   });
 </script>
 
 <canvas bind:this={el}></canvas>
+
+<style>
+
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+canvas {
+  width: 100vw;
+  height: 100vh;
+  display: block;
+}
+</style>
